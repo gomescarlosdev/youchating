a6d8f6e8e125e283dc0f85ed3051cc20e73359b2
gomescarlosdev <carlos.henrique.pro@outlook.com>
2023-03-06 23:11:55 -0300

    feat:(init project) :sparkles: initialyzing the project

---
+++ app/src/main/java/br/com/gomescarlosdev/youchating/view/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.com.gomescarlosdev.youchating.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.gomescarlosdev.youchating.R
+import br.com.gomescarlosdev.youchating.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
